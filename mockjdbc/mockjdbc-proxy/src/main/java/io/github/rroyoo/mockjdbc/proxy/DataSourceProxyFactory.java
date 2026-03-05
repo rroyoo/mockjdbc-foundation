83068e37f0b8563244968206a6c6c40e91f2fb21
rroyo <ramon.royo.es@gmail.com>
2026-03-05 23:01:03 +0100

    refactor: reboot modules

--- mockjdbc/mockjdbc-proxy/src/main/java/io/github/rroyoo/mockjdbc/proxy/DataSourceProxyFactory.java
+++
@@ -1,7 +0,0 @@
-package io.github.rroyoo.mockjdbc.proxy;
-
-import javax.sql.DataSource;
-
-public interface DataSourceProxyFactory {
-    DataSource create(DataSource originalDataSource);
-}
--- mockjdbc/mockjdbc-proxy/src/main/java/io/github/rroyoo/mockjdbc/proxy/QueryExecutionEventListener.java
+++
@@ -1,6 +0,0 @@
-package io.github.rroyoo.mockjdbc.proxy;
-
-public interface QueryExecutionEventListener {
-
-    void onQueryExecutionEvent(QueryExecutionEvent event);
-}
